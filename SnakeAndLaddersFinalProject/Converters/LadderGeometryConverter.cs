6c59f28dfb993620d387abed150e0af101f2297f
Onecap05 <zs23014095@estudiantes.uv.mx>
2025-11-06 16:46:59 -0600

    se añadieron clases en converters para los graficos de serpientes y escaleras

---
+++ SnakeAndLaddersFinalProject/Converters/LadderGeometryConverter.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace SnakeAndLaddersFinalProject.Converters
+{
+    public sealed class LadderGeometryConverter : IMultiValueConverter
+    {
+        private const double MIN_DISTANCE_TOLERANCE = 0.001;
+        private const double RAIL_OFFSET_FACTOR = 0.10;
+        private const double MAX_RAIL_OFFSET = 0.25;
+
+        private const double STEPS_PER_UNIT = 1.2;
+        private const int MIN_STEPS = 2;
+        private const int MAX_STEPS = 7;
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values == null || values.Length < 4)
+            {
+                return Geometry.Empty;
+            }
+
+            if (!TryGetDouble(values[0], out double startX) ||
+                !TryGetDouble(values[1], out double startY) ||
+                !TryGetDouble(values[2], out double endX) ||
+                !TryGetDouble(values[3], out double endY))
+            {
+                return Geometry.Empty;
+            }
+
+            var startPoint = new Point(startX, startY);
+            var endPoint = new Point(endX, endY);
+
+            double deltaX = endX - startX;
+            double deltaY = endY - startY;
+            double distance = Math.Sqrt((deltaX * deltaX) + (deltaY * deltaY));
+
+            if (distance < MIN_DISTANCE_TOLERANCE)
+            {
+                return new LineGeometry(startPoint, endPoint);
+            }
+
+            double directionX = deltaX / distance;
+            double directionY = deltaY / distance;
+
+            double perpendicularX = -directionY;
+            double perpendicularY = directionX;
+
+            double offset = distance * RAIL_OFFSET_FACTOR;
+            if (offset > MAX_RAIL_OFFSET)
+            {
+                offset = MAX_RAIL_OFFSET;
+            }
+
+            var rail1Start = new Point(
+                startX + (perpendicularX * offset),
+                startY + (perpendicularY * offset));
+
+            var rail1End = new Point(
+                endX + (perpendicularX * offset),
+                endY + (perpendicularY * offset));
+
+            var rail2Start = new Point(
+                startX - (perpendicularX * offset),
+                startY - (perpendicularY * offset));
+
+            var rail2End = new Point(
+                endX - (perpendicularX * offset),
+                endY - (perpendicularY * offset));
+
+            var geometry = new PathGeometry();
+
+            var rail1Figure = new PathFigure
+            {
+                StartPoint = rail1Start,
+                IsClosed = false,
+                IsFilled = false
+            };
+            rail1Figure.Segments.Add(new LineSegment(rail1End, true));
+            geometry.Figures.Add(rail1Figure);
+
+            var rail2Figure = new PathFigure
+            {
+                StartPoint = rail2Start,
+                IsClosed = false,
+                IsFilled = false
+            };
+            rail2Figure.Segments.Add(new LineSegment(rail2End, true));
+            geometry.Figures.Add(rail2Figure);
+
+            int stepCount = (int)(distance * STEPS_PER_UNIT);
+            if (stepCount < MIN_STEPS)
+            {
+                stepCount = MIN_STEPS;
+            }
+            else if (stepCount > MAX_STEPS)
+            {
+                stepCount = MAX_STEPS;
+            }
+
+            for (int i = 1; i < stepCount; i++)
+            {
+                double t = i / (double)stepCount;
+
+                double centerX = startX + (directionX * distance * t);
+                double centerY = startY + (directionY * distance * t);
+
+                var stepStart = new Point(
+                    centerX + (perpendicularX * offset),
+                    centerY + (perpendicularY * offset));
+
+                var stepEnd = new Point(
+                    centerX - (perpendicularX * offset),
+                    centerY - (perpendicularY * offset));
+
+                var stepFigure = new PathFigure
+                {
+                    StartPoint = stepStart,
+                    IsClosed = false,
+                    IsFilled = false
+                };
+
+                stepFigure.Segments.Add(new LineSegment(stepEnd, true));
+                geometry.Figures.Add(stepFigure);
+            }
+
+            return geometry;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        private static bool TryGetDouble(object value, out double result)
+        {
+            if (value is double doubleValue)
+            {
+                result = doubleValue;
+                return true;
+            }
+
+            if (value != null &&
+                double.TryParse(
+                    value.ToString(),
+                    NumberStyles.Any,
+                    CultureInfo.InvariantCulture,
+                    out double parsedValue))
+            {
+                result = parsedValue;
+                return true;
+            }
+
+            result = 0.0;
+            return false;
+        }
+    }
+}
---
+++ SnakeAndLaddersFinalProject/Converters/SnakeCurveGeometryConverter.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace SnakeAndLaddersFinalProject.Converters
+{
+    public sealed class SnakeCurveGeometryConverter : IMultiValueConverter
+    {
+        private const double MIN_DISTANCE_TOLERANCE = 0.001;
+        private const double CURVE_FACTOR = 0.25;
+        private const double MAX_CURVE_OFFSET = 1.20;
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values == null || values.Length < 5)
+            {
+                return Geometry.Empty;
+            }
+
+            if (!TryGetDouble(values[0], out double startX) ||
+                !TryGetDouble(values[1], out double startY) ||
+                !TryGetDouble(values[2], out double endX) ||
+                !TryGetDouble(values[3], out double endY))
+            {
+                return Geometry.Empty;
+            }
+
+            bool isLadder = false;
+
+            if (values[4] is bool ladderFlag)
+            {
+                isLadder = ladderFlag;
+            }
+
+            var startPoint = new Point(startX, startY);
+            var endPoint = new Point(endX, endY);
+
+            double deltaX = endX - startX;
+            double deltaY = endY - startY;
+            double distance = Math.Sqrt((deltaX * deltaX) + (deltaY * deltaY));
+
+            
+            if (distance < MIN_DISTANCE_TOLERANCE || isLadder)
+            {
+                return new LineGeometry(startPoint, endPoint);
+            }
+
+            double perpendicularX = -deltaY / distance;
+            double perpendicularY = deltaX / distance;
+
+            double oneThirdX = startX + (deltaX / 3.0);
+            double oneThirdY = startY + (deltaY / 3.0);
+
+            double twoThirdsX = startX + (2.0 * deltaX / 3.0);
+            double twoThirdsY = startY + (2.0 * deltaY / 3.0);
+
+            double midX = (startX + endX) * 0.5;
+            double midY = (startY + endY) * 0.5;
+            var midPoint = new Point(midX, midY);
+
+            double offset = distance * CURVE_FACTOR;
+
+            if (offset > MAX_CURVE_OFFSET)
+            {
+                offset = MAX_CURVE_OFFSET;
+            }
+
+            var controlPoint1 = new Point(
+                oneThirdX + (perpendicularX * offset),
+                oneThirdY + (perpendicularY * offset));
+
+            var controlPoint2 = new Point(
+                twoThirdsX - (perpendicularX * offset),
+                twoThirdsY - (perpendicularY * offset));
+
+            var figure = new PathFigure
+            {
+                StartPoint = startPoint,
+                IsClosed = false,
+                IsFilled = false
+            };
+
+            figure.Segments.Add(
+                new QuadraticBezierSegment(controlPoint1, midPoint, true));
+
+            figure.Segments.Add(
+                new QuadraticBezierSegment(controlPoint2, endPoint, true));
+
+            var geometry = new PathGeometry();
+            geometry.Figures.Add(figure);
+
+            return geometry;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        private static bool TryGetDouble(object value, out double result)
+        {
+            if (value is double doubleValue)
+            {
+                result = doubleValue;
+                return true;
+            }
+
+            if (value != null &&
+                double.TryParse(
+                    value.ToString(),
+                    NumberStyles.Any,
+                    CultureInfo.InvariantCulture,
+                    out double parsedValue))
+            {
+                result = parsedValue;
+                return true;
+            }
+
+            result = 0.0;
+            return false;
+        }
+    }
+}
