4ba80e7e85d152ed32880318238a5a003b2af390
Onecap05 <zs23014095@estudiantes.uv.mx>
2025-11-05 20:14:17 -0600

    se modifico la vista de las serpientes

---
+++ SnakeAndLaddersFinalProject/Converters/SnakeHeadOffsetConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SnakeAndLaddersFinalProject.Converters
+{
+    public sealed class SnakeHeadOffsetConverter : IValueConverter
+    {
+        private const double HEAD_RADIUS = 0.20; 
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double coord)
+            {
+                
+                return coord - HEAD_RADIUS;
+            }
+
+            return 0d;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
